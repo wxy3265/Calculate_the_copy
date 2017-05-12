c1c26a86028ed365a06ca7a6e0665a23ab032940
wxy3265 <546668033@qq.com>
2017-05-12 22:40:52 +0800

    基础输入

--- app/src/main/java/com/example/wxy/calculatethecopy/MainActivity.java
+++ app/src/main/java/com/example/wxy/calculatethecopy/MainActivity.java
@@ -2,12 +2,74 @@ package com.example.wxy.calculatethecopy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int bs = 1;//抄写遍数
+    int days = 3;//剩余天数
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.text);//结果输出View
+        TextView editText = (TextView) findViewById(R.id.edittext);//遍数输入View
+        TextView editText1 = (TextView) findViewById(R.id.edittext1);//天数输入View
+        Button button = (Button) findViewById(R.id.button);//确认按钮
+        Button button_add = (Button) findViewById(R.id.button_add);//增加按钮
+        Button button_less = (Button) findViewById(R.id.button_less);//减少按钮
+        Button button_add1 = (Button) findViewById(R.id.button_add1);//增加按钮
+        Button button_less1 = (Button) findViewById(R.id.button_less1);//减少按钮
+
+        button_add.setOnClickListener(this);//增加逻辑计算
+        button_less.setOnClickListener(this);//减少逻辑计算
+        button_add1.setOnClickListener(this);//增加逻辑计算
+        button_less1.setOnClickListener(this);//减少逻辑计算
+        button.setOnClickListener(this);//确认逻辑计算
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView textView = (TextView) findViewById(R.id.text);//结果输出View
+        TextView editText = (TextView) findViewById(R.id.edittext);//遍数输入View
+        TextView editText1 = (TextView) findViewById(R.id.edittext1);//天数输入View
+
+        switch (v.getId()) {
+            case R.id.button_add:
+                bs ++;
+                editText.setText(String.valueOf(bs));
+                break;
+            case R.id.button_less:
+                if (bs > 1)
+                    bs --;
+                else
+                    Toast.makeText(MainActivity.this, "已达到下限",
+                            Toast.LENGTH_SHORT).show();
+                editText.setText(String.valueOf(bs));
+                break;
+            case R.id.button_add1:
+                days ++;
+                editText1.setText(String.valueOf(days));
+                break;
+            case R.id.button_less1:
+                if (days > 1)
+                    days --;
+                else
+                    Toast.makeText(MainActivity.this, "已达到下限",
+                            Toast.LENGTH_SHORT).show();
+                editText1.setText(String.valueOf(days));
+                break;
+            case  R.id.button:
+                Toast.makeText(MainActivity.this, "You Clicked 计算",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
